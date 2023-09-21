d3412b2626a2e093300c103f441d9dd7c796508a
vinithecsar <viniciuscesarbrito@gmail.com>
2023-09-21 18:09:05 -0300

    atualização do título

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Porta Premida 🚪',
+  title: 'Porta Premiada 🚪',
   description: 'Gerado por create next app',
 };
 
